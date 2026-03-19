c0932f43b20945cc781f704cbdd8f72a5b8fd239
timonbat0n <timonbat0n@icloud.com>
2026-03-19 23:13:34 +0300

    Create maifest.js

---
+++ maifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Вожатский чемоданчик",
+  "short_name": "Чемоданчик",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#fdf6e3",
+  "theme_color": "#e74c3c",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
